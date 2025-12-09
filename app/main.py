6fbb585c76da137ee2d9eee9423b0ff3776d508a
Hernan Ogasawara <hogasawara@afip.gob.ar>
2025-12-09 15:53:13 -0300

    Endpoint de subida de archivo con FastAPI.

--- app/main.py
+++ app/main.py
@@ -1,3 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/api/camino_mas_corto")
+async def camino_mas_corto(file: UploadFile = File(...)):
+    try:
+        with open(file.filename, "wb") as myFile:
+            content = await file.read()
+            myFile.write(content)
+            myFile.close()
+            print(file.filename)
+            return JSONResponse(content={
+                "saved": True
+            }, status_code=200)
+    except FileNotFoundError as e:
+        print(e)
+        return JSONResponse(content={
+            "saved": False
+        }, status_code=404)
